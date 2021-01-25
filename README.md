e123e062e765bf605e2ed68dc501ab42c7335235
Anthony Kung <hi@anth.dev>
2021-01-24 20:30:41 -0800

    Initial Release

---
+++ README.md
@@ -0,0 +1,35 @@
+# GitSave README
+
+GitSave automatically commit to Git on save.
+
+## Features
+
+Does what the description said, runs `git commit -am "Update at {{ Timestamp }}"` on save.
+
+## Usage
+
+Enable GitSave using Command Palette `GitSave: Enable` - Disabled by default
+
+Disable GitSave using Command Palette `GitSave: Disable`
+
+Run GPG Test using Command Palette `GitSave: GPG Test`
+
+Note: If commit with GPG, cache GPG passphrase and run GPG Test before using GitSave.
+
+## Requirements
+
+You'll just need VS Code with Git enabled.
+
+## Extension Settings
+
+Configuration currently unavailable
+
+## Known Issues
+
+No known issues
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of GitSafe
---
+++ extension.js
@@ -0,0 +1,88 @@
+/******************************************
+ *                                        *
+ * Name: extension.js                     *
+ * Description: GitSafe VS Code Extension *
+ * Date: January 24, 2021                 *
+ * Created by: Anthony Kung               *
+ * Author URL: https://anth.dev           *
+ * License: Apache-2.0 (See LICENSE.md)   *
+ *                                        *
+ ******************************************/
+
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const { exec, spawn, spawnSync } = require('child_process');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('\x1b[92m>_ GitSave Activated!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('gitsave.helloWorld', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from GitSave!');
+	});
+
+	let status = 0;
+
+	let save = vscode.workspace.onDidSaveTextDocument((TextDocument) => {
+		if (TextDocument.uri.scheme === "file" && status === 1) {
+			let curry = new Date().toUTCString();
+			let cmd = exec('cd ' + vscode.workspace.workspaceFolders[0].uri.path + ' && git commit ' + TextDocument.uri.path + ' -m "Update at ' + curry + '"', (err, stdout, stderr) => {
+				console.log('\x1b[94m>_ GitSave Return:\n' + stdout);
+				console.log('\x1b[91m>_ Git Error:\n' + stderr);
+				if (err) {
+					console.log('\x1b[91m>_ GitSave Error:\n' + err);
+				}
+			});
+		}
+	});
+
+	let gpgtest = vscode.commands.registerCommand('gitsave.gpgtest', function () {
+		let cmd = vscode.window.createTerminal({
+			name: "GitSave GPG Test",
+			cwd: vscode.workspace.workspaceFolders[0].uri.path,
+			hideFromUser: false
+		});
+
+		cmd.sendText('echo "GitSave Test" | gpg --clearsign\n');
+
+		cmd.show();
+	});
+
+	let disable = vscode.commands.registerCommand('gitsave.disable', function () {
+		status = 0;
+	});
+
+	let enable = vscode.commands.registerCommand('gitsave.enable', function () {
+		status = 1;
+	});
+
+	context.subscriptions.push(
+		gpgtest,
+		enable,
+		disable
+	);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
